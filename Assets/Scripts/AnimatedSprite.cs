5903ddd3e53bc653b3c19ff79c17732ee55658ec
ivankadyka <ivankadyko@gmail.com>
2024-03-18 10:54:20 +0100

    init sprite atlas

--- Assets/Scripts/AnimatedSprite.cs
+++ Assets/Scripts/AnimatedSprite.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
+using UnityEngine.U2D;
 
 [RequireComponent(typeof(SpriteRenderer))]
 public class AnimatedSprite : MonoBehaviour
 {
-    public Sprite[] sprites;
     private SpriteRenderer spriteRenderer;
     private int frame;
+
+    [SerializeField] 
+    private SpriteAtlas _spriteAtlas;
+    
+    [SerializeField] 
+    private string[] _spriteNames;
+
+    private Sprite[] _sprites;
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+
+        _sprites = new Sprite[_spriteNames.Length];
+        
+        for (int i = 0; i < _spriteNames.Length; i++)
+        {
+            _sprites[i] = _spriteAtlas.GetSprite(_spriteNames[i]);
+        }
     }
 
     private void OnEnable()
@@ -26,12 +41,12 @@ public class AnimatedSprite : MonoBehaviour
     {
         frame++;
 
-        if (frame >= sprites.Length) {
+        if (frame >= _sprites.Length) {
             frame = 0;
         }
 
-        if (frame >= 0 && frame < sprites.Length) {
-            spriteRenderer.sprite = sprites[frame];
+        if (frame >= 0 && frame < _sprites.Length) {
+            spriteRenderer.sprite = _sprites[frame];
         }
 
         Invoke(nameof(Animate), 1f / 5);
